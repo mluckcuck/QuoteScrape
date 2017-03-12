4277f527947a4944cc6cde131b7a334a8348b159
Matt Luckcuck <ml881@york.ac.uk>
2017-03-12 18:48:53 +0000

    Added terminal flags to give url and search name. Also added (defaly) read urls from file option.

--- quoteScrape.py
+++ quoteScrape.py
@@ -1,60 +1,69 @@
 #Quote Scrape - Python3
+#12th MArch 2017
 from lxml import html
 import requests
 import json
 import sys
+import argparse
 
 print("+++ Quote Scrape +++ ")
 
-for arg in sys.argv:
-    print(arg)
+URLS_JSON = "urls.json"
 
-url = ''
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", help="The URL you wish to check")
+parser.add_argument("--searchName", help="Optional, names to return quotes for")
+args = parser.parse_args()
+
+urlArg = args.url
 searchName = ''
+if args.searchName:
+     searchName = args.searchName
 
-if len(sys.argv) == 2:
-    url = sys.argv[1]
-elif len(sys.argv) == 3:
-    url = sys.argv[1]
-    searchName = sys.argv[2]
-else:
-    print("Please specify URL in quotes")
+quotesDictList = []
 
-
-page = requests.get(url)
-tree = html.fromstring(page.content)
-
-
-filmName = tree.xpath('//*[@id="main"]/div[1]/div[1]/div/h3/a/text()')[0]
-
-quotesList = tree.xpath('//*[@id="quotes_content"]/div[2]/div/div[@class="sodatext"]/p')
-
-
-def buildQuoteDict(name, quote):
+def buildQuoteDict(filmName, quote):
     quoteDict = {}
     quoteDict['Film']=filmName
     quoteDict['Quote']=quote[1].replace('\n','').replace(':','')
 
     return quoteDict
 
-quotesDictList = []
+def outputToFile(quotesDictList):
+    json_data = json.dumps(quotesDictList)
+    quoteFile = open("quotes.json", 'w')
+    json.dump(quotesDictList, quoteFile)
+    quoteFile.close()
 
-for q in quotesList:
-    name = q.xpath('a/span/text()')
-    quote  = q.xpath('text()')
+def getQuotesFrom(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
 
-    if searchName != '':
-        if searchName in name:
+    filmName = tree.xpath('//*[@id="main"]/div[1]/div[1]/div/h3/a/text()')[0]
+    quotesList = tree.xpath('//*[@id="quotes_content"]/div[2]/div/div[@class="sodatext"]/p')
+
+    for q in quotesList:
+        name = q.xpath('a/span/text()')
+        quote  = q.xpath('text()')
+
+        if args.searchName:
+            if searchName in name:
+                quotesDictList.append(buildQuoteDict(filmName, quote))
+        else:
             quotesDictList.append(buildQuoteDict(filmName, quote))
-    else:
-        quotesDictList.append(buildQuoteDict(filmName, quote))
 
-json_data = json.dumps(quotesDictList)
+if args.url:
+    print("Using Given URL")
+    getQuotesFrom(urlArg)
+else:
+    print("using File")
+    urls=open(URLS_JSON).read()
+    urlList = json.loads(urls)
+
+    for aURL in urlList:
+        getQuotesFrom(aURL)
 
 
-quoteFile = open("quotes.json", 'w')
-json.dump(quotesDictList, quoteFile)
+outputToFile(quotesDictList)
 
-print(type(json_data))
-print(json_data)
-print("***")
+print("+++ Done +++")
