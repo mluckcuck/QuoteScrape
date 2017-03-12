a4e484469173d3775a6b6a312ebe9aafe406a6e9
Matt Luckcuck <ml881@york.ac.uk>
2017-03-12 15:04:59 +0000

    Basci working output, with command line options

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # QuoteScrape
 A Simple Python script that scrapes a given IMDB 'Quotes' page and returns a JSON
 list of objects containing the film name and quote.
+
+##Usage
+
+python3 quoteScrape.py "URL" ["Search Name"]
+
+URL = the url of the IMDB Quotes page to scrapes
+Search Name = the name of the character to return quotes for (optional)
---
+++ quoteScrape.py
@@ -0,0 +1,61 @@
+#Quote Scrape - Python3
+from lxml import html
+import requests
+import json
+import sys
+
+print("+++ Quote Scrape +++ ")
+
+for arg in sys.argv:
+    print(arg)
+
+url = ''
+searchName = ''
+
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+elif len(sys.argv) == 3:
+    url = sys.argv[1]
+    searchName = sys.argv[2]
+else:
+    print("Please specify URL in quotes")
+
+
+page = requests.get(url)
+tree = html.fromstring(page.content)
+
+
+filmName = tree.xpath('//*[@id="main"]/div[1]/div[1]/div/h3/a/text()')[0]
+
+quotesList = tree.xpath('//*[@id="quotes_content"]/div[2]/div/div[@class="sodatext"]/p')
+
+#for each q in quotesList select the text in each a in each p (that has search name)
+
+#print(quotesList)
+
+
+def buildQuoteDict(name, quote):
+    quoteDict = {}
+    quoteDict['Film']=filmName
+    quoteDict['Quote']=quote[1].replace('\n','').replace(':','')
+
+    return quoteDict
+
+quotesDictList = []
+
+for q in quotesList:
+    name = q.xpath('a/span/text()')
+    quote  = q.xpath('text()')
+
+    if searchName != '':
+        if searchName in name:
+            quotesDictList.append(buildQuoteDict(filmName, quote))
+    else:
+        quotesDictList.append(buildQuoteDict(filmName, quote))
+
+json_data = json.dumps(quotesDictList)
+
+
+print(type(json_data))
+print(json_data)
+print("***")
